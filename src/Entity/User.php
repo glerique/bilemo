34630cdb7289ec7f0c6427f0895932ce99e1ef74
glerique <gael.lerique@gmail.com>
2020-11-03 14:31:58 +0100

    Add ApiResource annotations and asserts

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,27 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ApiResource()
+ * @ApiResource(   
+ *  collectionOperations={
+ *          "get" = {"normalization_context"={"groups"="users:list"}}, 
+ *          "post" = {"denormalization_context" ={"groups" = {"users:write"},
+ *          "disable_type_enforcement"=true}}},
+ *  itemOperations={
+ *          "get"={"normalization_context"={"groups"="users:item"}},"delete"},
+ *  attributes={
+ *           "pagination_items_per_page"=10,
+ *            "order": {"id":"desc"}} 
+ * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity("email", message="Un autre utilisateur possède déjà cette adresse email")
  */
 class User
 {
@@ -16,42 +30,85 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"users:list", "users:item"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"users:list", "users:item", "users:write"})
+     * @Assert\NotBlank(message="Le prénom est obligatoire")
+     * @Assert\Length(
+     *      min=3, 
+     *      minMessage="Le prénom doit faire entre 3 et 255 caractères", 
+     *      max=255, 
+     *      maxMessage="Le prénom doit faire entre 3 et 255 caractères")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"users:list", "users:item", "users:write"})
+     * @Assert\NotBlank(message="Le nom de famille est obligatoire")
+     * @Assert\Length(
+     *      min=3, 
+     *      minMessage="Le nom de famille doit faire entre 3 et 255 caractères", 
+     *      max=255, 
+     *      maxMessage="Le nom de famille doit faire entre 3 et 255 caractères")
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"users:item", "users:write"})
+     * @Assert\NotBlank(message="L'adresse est obligatoire")
+     * @Assert\Length(
+     *      min=3, 
+     *      minMessage="L'adresse doit faire entre 5 et 255 caractères", 
+     *      max=255, 
+     *      maxMessage="L'adresse doit faire entre 5 et 255 caractères")
      */
     private $address;
 
     /**
      * @ORM\Column(type="integer")
+     * @Groups({"users:item", "users:write"})
+     * @Assert\NotBlank(message="Le code postal est obligatoire")
+     * @Assert\Type(
+     *      type="integer", 
+     *      message="Le code postal est incorrect")
+     * @Assert\Length(
+     *      min="5",     *            
+     *      max="5",     
+     *      exactMessage="Le code postal est incorrect"
+     * )
      */
     private $postCode;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"users:item", "users:write"})
+     * @Assert\NotBlank(message="La ville est obligatoire")
+     * @Assert\Length(
+     *      min=3, 
+     *      minMessage="La ville doit faire entre 2 et 255 caractères", 
+     *      max=255, 
+     *      maxMessage="La ville doit faire entre 2 et 255 caractères")
      */
     private $city;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"users:list", "users:item", "users:write"})
+     * @Assert\NotBlank(message="L'email doit être renseigné !")
+     * @Assert\Email(message="L'adresse email doit avoir un format valide !")
      */
     private $email;
 
     /**
      * @ORM\ManyToOne(targetEntity=Client::class, inversedBy="Users")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"users:write"})
      */
     private $client;
 
@@ -101,7 +158,7 @@ class User
         return $this->postCode;
     }
 
-    public function setPostCode(int $postCode): self
+    public function setPostCode($postCode): self
     {
         $this->postCode = $postCode;
 
